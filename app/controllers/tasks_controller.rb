79203100b0e1249062d4ef9275d59e37d2784c26
kamoto0427 <takahiro.08039@gmail.com>
2021-01-18 23:00:16 +0900

    一覧ページとタスク作成ページの仮レイアウト

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,21 @@
 class TasksController < ApplicationController
+  def index
+    # @tasks = Task.all
+  end
+
+  def new
+    # @task = Task.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
